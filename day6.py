780460e5064a99260b66f55a7cbaba1b3a0aaef7
blhowell <blhowell@aggienetwork.com>
2017-12-08 18:30:20 -0600

    day 6, part 1.

---
+++ day6.py
@@ -0,0 +1,66 @@
+"""
+Advent of Code 2017
+Python
+
+
+"To do this, it removes all of the blocks from the selected bank, then moves to the next (by index) memory bank and inserts one of the blocks.
+It continues doing this until it runs out of blocks; if it reaches the last memory bank, it wraps around to the first one."
+"""
+import csv
+import copy
+
+
+def part1():
+    # with open("day6_example_input.txt", "r") as f:
+    with open("day6_input.txt", "r") as f:
+        nums = [int(num) for num in f.read().strip().split("\t")]
+
+    # to keep track of previous number sequences had
+    old_nums = []
+    iterations = 0
+
+    while True:
+        # input()
+        iterations += 1
+
+        # oh... right.... lists are mutable...
+        old_nums.append(copy.deepcopy(nums))
+
+        where_the_giver_is = nums.index(max(nums))
+        gift = nums[where_the_giver_is]
+
+        # print(nums)
+        # print("where: {}\tval: {}\t\tgift: {}".format(
+        #     where_the_giver_is, nums[where_the_giver_is], gift))
+
+        # begin gift giving
+        current = where_the_giver_is
+        nums[where_the_giver_is] = 0
+        while gift > 0:
+            try:
+                # move on to the end of the list giving
+                current += 1
+                gift -= 1
+                nums[current] += 1  # point of exception
+                # print(nums)
+                # print(gift)
+            except IndexError:
+                # go to start of list!
+                current = 0
+                nums[current] += 1
+                # print(nums)
+                # print(gift)
+
+
+        # if this number sequence has appeared before
+        if nums in old_nums:
+            # print(old_nums)
+            return iterations
+
+
+def part2():
+    pass
+
+
+print("Part One...", part1())
+print("Part Two...", part2())
